25ef606cf6a6ac2bd1ca91410bdf77aef947f693
Rishikesh <146565427+rishikeshghemud@users.noreply.github.com>
2025-10-06 01:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
 
-class Square extends Animal {}
+		if(height<0 || width<0) {
+			return "Please enter positive values";
+		}
+		
+		this.width=width;
+		this.height=height;
+	}
+
+	get getWidth() {
+		return this.width;
+	}
+
+	get getHeight() {
+		return this.height;
+	}
+
+	getArea() {
+		return this.width * this.height;
+	}
+}
+
+class Square extends Animal {
+	constructor(width) {
+		super(width, width);
+	}
+
+	getPerimeter() {
+		return this.width*4;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
